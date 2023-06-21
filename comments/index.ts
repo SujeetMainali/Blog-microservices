7012556e589d11c776ca577d142990919c94e283
Sujeet Mainali <mainalisujeet@gmail.com>
2023-06-22 03:10:32 +0545

    comment service app setup

--- comments/index.ts
+++ comments/index.ts
@@ -1,1 +1,17 @@
-console.log('hello');
+import express from 'express'
+import bodyParser from 'body-parser'
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/posts/:id/comments', (req, res)=>{
+    res.json({
+        name : "sujeet"
+    })
+})
+
+
+
+app.listen(3000, ()=>{
+    console.log(`http://localhost:3000`);
+    
+})
