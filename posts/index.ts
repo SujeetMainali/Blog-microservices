823fdb320db509b73895d3c1d60430a4c60fdb64
Sujeet Mainali <mainalisujeet@gmail.com>
2023-06-21 18:48:39 +0545

    project setup for post service

--- posts/index.ts
+++ posts/index.ts
@@ -1,1 +1,28 @@
-console.log("hello hello");
+import express from 'express'
+import {v4 as uuid} from 'uuid'
+import bodyParser from 'body-parser'
+
+const app = express()
+app.use(bodyParser.json())
+const posts = {}
+
+app.get('/posts',(req, res)=>{
+    res.send(posts)
+})
+
+app.post('/post',(req, res)=>{
+    const id = uuid()
+   const {title} = req.body
+   posts[id] = {
+    id, title
+   }
+    
+   res.status(200).send(posts[id])
+
+})
+
+
+app.listen(4000, ()=>{
+    console.log(`http://localhost:4000`);
+    
+})
