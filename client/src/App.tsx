f318989efa6431271f963a24dfb67dd4850c4918
Sujeet Mainali <mainalisujeet@gmail.com>
2023-06-24 21:15:32 +0545

    post list component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import PostCreate from './components/PostCreate';
+import PostList from './components/PostList';
 function App() {
   return (
     <>
      <PostCreate />
+     <PostList />
     </>
   );
 }
