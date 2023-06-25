c157edcccc3756217300b5fd926116990702442b
Sujeet Mainali <mainalisujeet@gmail.com>
2023-06-25 17:26:45 +0545

    added toast on comment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { ToastContainer } from 'react-toastify';
 import './App.css'
 import PostCreate from './components/PostCreate';
 import PostList from './components/PostList';
+ import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <div>
