7130f0682942d4af34af3638a445ce4dddfbb25d
Sujeet Mainali <mainalisujeet@gmail.com>
2023-06-25 16:43:27 +0545

    render posts

--- client/src/components/PostList.tsx
+++ client/src/components/PostList.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-const PostList = () => {
-  return (
-    <div>PostList</div>
-  )
+const getPosts =async () => {
+  const res = await axios.get('http://localhost:4000/posts')
+  console.log(res?.data, 'asdhfjkashdgfjkhasd');
+  
+  return res?.data
 }
+const PostList = () => {
 
-export default PostList
+  const fetchPosts = useQuery({
+    queryKey: ['posts'],
+    queryFn: getPosts
+  })
+  console.log("sdhfkasdhf", fetchPosts);
+  if(fetchPosts.isLoading) return <div>Loading.....</div>
+  return (
+    <div className="flex flex-wrap flex-row gap-4 mt-5 ml-6">
+      {Object.values(fetchPosts.data).map((post: any) => {
+        return (
+          <div
+            key={post.id}
+            className=" w-56 h-56 border-2 border-black text-center "
+          >
+            <p className=" font-bold text-xl">{post.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default PostList;
